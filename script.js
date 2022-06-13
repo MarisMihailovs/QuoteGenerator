2fbe36736035169180f13e8b9fc33a4b6e32490f
MarisMihailovs <marismihailovs@gmail.com>
2022-06-13 13:48:55 +0300

    added new background and connected api

---
+++ script.js
@@ -0,0 +1,36 @@
+function init() {
+
+    document.body.style.background = 'url(bonnie1.jpg) no-repeat center center'
+}
+
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQuotes = [];
+
+// show new quote 
+function newQuote() {
+    // pick random quote
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
+}
+
+// Get quotes from API
+async function getQuotes() {
+    const apiUrl = "https://type.fit/api/quotes";
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+
+    } catch (error) {
+        // catch error here
+    }
+}
+
+// on load
+getQuotes();
